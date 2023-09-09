5ce471af009b69dc08e4dbb20a6d6b9cc69fd2a9
Dmitriy Kolmakov <kolmakov125@gmail.com>
2023-09-10 00:06:59 +0300

    2

--- lab1/handler.php
+++ lab1/handler.php
@@ -1,10 +1,24 @@
 <?php
-  echo "Hello world!"; // Your code here
+
+session_start();
+
+const table_head = "
+<table>
+    <tr>
+        <td>X</td>
+        <td>Y</td>
+        <td>R</td>
+        <td>Результат</td>
+        <td>Текущее время</td>
+        <td>Время работы</td>
+    </tr>
+</table>";
+const table_tail = '</table>';
+
+if (isset($_GET)) {
+    echo("X:" . (float)htmlspecialchars($_GET['delta_x']));
+    print("Y:" . (float)htmlspecialchars($_GET['delta_y']));
+    print("Z:" . (float)htmlspecialchars($_GET['delta_z']));
+}
+
 ?>
-
-
-    <!-- if (isset($_GET)) {
-        echo("X:" . (float)htmlspecialchars($_GET['delta_x']));
-        print("Y:" . (float)htmlspecialchars($_GET['delta_y']));
-        print("Z:" . (float)htmlspecialchars($_GET['delta_z']));
-    } -->

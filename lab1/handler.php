e2f52a4c6994ff928e8aca17817aedc4341e48c1
Dmitriy Kolmakov <kolmakov125@gmail.com>
2023-09-10 00:30:49 +0300

    3

--- lab1/handler.php
+++ lab1/handler.php
@@ -15,10 +15,57 @@ const table_head = "
 </table>";
 const table_tail = '</table>';
 
-if (isset($_GET)) {
-    echo("X:" . (float)htmlspecialchars($_GET['delta_x']));
-    print("Y:" . (float)htmlspecialchars($_GET['delta_y']));
-    print("Z:" . (float)htmlspecialchars($_GET['delta_z']));
+$current_time = date("H:i:s");
+$start_time = microtime(true);
+
+
+if (!isset($_SESSION["results"])) {
+    $_SESSION["results"] = array();
+}
+
+if (isset($_GET["delta_x"]) && isset($_GET["delta_y"]) && isset($_GET["delta_r"])) {
+    $delta_x = floatval($_GET["delta_x"]);
+    $delta_y = floatval($_GET["delta_y"]);
+    $delta_r = floatval($_GET["delta_r"]);
+
+    $execution_time = microtime(true) - $start_time;
+
+    $new_result = array(
+        "delta_x" => $delta_x,
+        "delta_y" => $delta_y,
+        "delta_r" => $delta_r,
+        "success" => true,
+        "current_time" => $current_time,
+        "execution_time" => $execution_time
+    );
+
+    $_SESSION["results"][] = $new_result;
+
+    echo json_encode(array(
+        "html" => RenderTable()
+    ));
+    exit();
+}
+
+function RenderTable() : string {
+    $final_table = "";
+
+    $final_table .= table_head;
+
+    foreach ($_SESSION["results"] as $row) {
+        $final_table .= "<tr>";
+        $final_table .= "<td>" . $row["delta_x"] . "</td>";
+        $final_table .= "<td>" . $row["delta_y"] . "</td>";
+        $final_table .= "<td>" . $row["delta_r"] . "</td>";
+        $final_table .= "<td>" . $row["success"] . "</td>";
+        $final_table .= "<td>" . $row["current_time"] . "</td>";
+        $final_table .= "<td>" . $row["execution_time"] . "</td>";
+        $final_table .= "</tr>";
+    }
+
+    $final_table .= table_tail;
+
+    return $final_table;
 }
 
 ?>

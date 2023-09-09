9cd36937afa77fdeaadfdfc5bf71fcc54221beea
Dmitriy Kolmakov <kolmakov125@gmail.com>
2023-09-09 19:56:33 +0300

    1

---
+++ lab1/handler.php
@@ -0,0 +1,10 @@
+<?php
+  echo "Hello world!"; // Your code here
+?>
+
+
+    <!-- if (isset($_GET)) {
+        echo("X:" . (float)htmlspecialchars($_GET['delta_x']));
+        print("Y:" . (float)htmlspecialchars($_GET['delta_y']));
+        print("Z:" . (float)htmlspecialchars($_GET['delta_z']));
+    } -->

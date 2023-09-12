d40e2732a2c7d76ac94d43be52ab2c80e226f445
Dmitriy Kolmakov <kolmakov125@gmail.com>
2023-09-12 16:20:12 +0300

    16

--- lab1/index.php
+++ lab1/index.php
@@ -1,5 +1,11 @@
 <!doctype html>
 
+<script>    
+    if(typeof window.history.pushState == 'function') {
+        window.history.pushState({}, "Hide", '<?php echo $_SERVER['PHP_SELF'];?>');
+    }
+</script>
+
 <?php
 
 // start session

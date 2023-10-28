027803a030bf1924a0489019b8d844c2631822e2
Dmitriy Kolmakov <kolmakov125@gmail.com>
2023-10-28 23:14:16 +0300

    add sample code

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HelloWorldServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+    }
 }

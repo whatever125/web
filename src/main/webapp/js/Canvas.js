c0bbc681de404edb25db3208652321e57637fdd2
Dmitriy Kolmakov <kolmakov125@gmail.com>
2023-11-05 20:16:13 +0300

    Rewrite canvas for custom radius

--- src/main/webapp/js/Canvas.js
+++ src/main/webapp/js/Canvas.js
@@ -1,4 +1,7 @@
 export default class Canvas {
+    static MAX_RADIUS = 5;
+    static WIDTH_PROPORTION = 3 / 16;
+
     id;
     canvas;
     width;
@@ -14,11 +17,11 @@ export default class Canvas {
     }
 
     define() {
-        this.width = window.innerWidth * 3 / 16;
-        this.height = this.width;
+        this.width = window.innerWidth * Canvas.WIDTH_PROPORTION;
+        this.height = window.innerWidth * Canvas.WIDTH_PROPORTION;
 
-        canvas.width = this.width;
-        canvas.height = this.height;
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
     }
 
     clear() {
@@ -36,21 +39,19 @@ export default class Canvas {
     }
 
     drawAreas() {
-        var ctx = this.canvas.getContext('2d');
+        // init
+        const ctx = this.canvas.getContext('2d');
 
-        // ctx.c
+        const r_pc = (this.radius / Canvas.MAX_RADIUS) * 0.4;
+        ctx.lineWidth = this.width * (2 / 300);
 
-        var r_pc = (this.radius / 3) * 0.4;
-        ctx.lineWidth = this.width * 2 / 300;
-
-        // blue areas
-        var area_color = "#f9b17a";
+        const area_color = "#f9b17a";
         ctx.fillStyle = area_color;
         ctx.strokeStyle = area_color;
 
         ctx.fillRect((0.1 + (0.4 - r_pc) + r_pc / 2) * this.width, this.height / 2, (r_pc / 2) * this.width, r_pc * this.height);
 
-        this.drawTriangle(ctx, {x: (0.1 + (0.4 - r_pc)) * this.width,  y: this.height / 2}, {x: this.width / 2,  y: this.height / 2}, {x: this.width / 2,  y: (0.1 + (0.4 - r_pc)) * this.height}, true);
+        this.drawTriangle(ctx, {x: (0.5 - r_pc) * this.width,  y: this.height / 2}, {x: this.width / 2,  y: this.height / 2}, {x: this.width / 2,  y: (0.5 - r_pc) * this.height}, true);
 
         ctx.beginPath();
         ctx.moveTo(this.width / 2, this.height / 2);
@@ -61,7 +62,8 @@ export default class Canvas {
     }
 
     drawGrid() {
-        var ctx = this.canvas.getContext('2d');
+        // init
+        const ctx = this.canvas.getContext('2d');
 
         ctx.lineWidth = this.width * 2 / 500;
         ctx.font = "18px Verdana"; // todo
@@ -69,41 +71,45 @@ export default class Canvas {
         ctx.fillStyle = "#ffffff";
         ctx.strokeStyle = "#ffffff";
 
-        // grid
-        var arrow_height = 0.05 * this.width;
+        const arrow_height = 0.05 * this.width;
 
+        // GRID
+        // vertical axis
         this.drawLine(ctx, this.width / 2, 0, this.width / 2, this.height);
 
+        // vertical axis arrow
         this.drawLine(ctx, this.width / 2, 0, this.width / 2 - arrow_height / 2, arrow_height);
         this.drawLine(ctx, this.width / 2, 0, this.width / 2 + arrow_height / 2, arrow_height);
 
-        this.drawLine(ctx, this.width / 2 - 0.01 * this.width, (0.1 + 0 * 0.8 / 6) * this.height, this.width / 2 + 0.01 * this.width, (0.1 + 0 * 0.8 / 6) * this.height);
-        this.drawLine(ctx, this.width / 2 - 0.01 * this.width, (0.1 + 1 * 0.8 / 6) * this.height, this.width / 2 + 0.01 * this.width, (0.1 + 1 * 0.8 / 6) * this.height);
-        this.drawLine(ctx, this.width / 2 - 0.01 * this.width, (0.1 + 2 * 0.8 / 6) * this.height, this.width / 2 + 0.01 * this.width, (0.1 + 2 * 0.8 / 6) * this.height);
-        this.drawLine(ctx, this.width / 2 - 0.01 * this.width, (0.1 + 4 * 0.8 / 6) * this.height, this.width / 2 + 0.01 * this.width, (0.1 + 4 * 0.8 / 6) * this.height);
-        this.drawLine(ctx, this.width / 2 - 0.01 * this.width, (0.1 + 5 * 0.8 / 6) * this.height, this.width / 2 + 0.01 * this.width, (0.1 + 5 * 0.8 / 6) * this.height);
-        this.drawLine(ctx, this.width / 2 - 0.01 * this.width, (0.1 + 6 * 0.8 / 6) * this.height, this.width / 2 + 0.01 * this.width, (0.1 + 6 * 0.8 / 6) * this.height);
+        // vertical axis marks
+        for (let i = 0; i <= Canvas.MAX_RADIUS * 2; i ++) {
+            if (i === Canvas.MAX_RADIUS) continue;
+            this.drawLine(ctx, this.width / 2 - 0.01 * this.width, (0.1 + i * 0.8 / (Canvas.MAX_RADIUS * 2)) * this.height,
+                this.width / 2 + 0.01 * this.width, (0.1 + i * 0.8 / (Canvas.MAX_RADIUS * 2)) * this.height);
+        }
 
+        // horizontal axis
         this.drawLine(ctx, 0, this.height / 2, this.width, this.height / 2);
 
+        // horizontal axis arrow
         this.drawLine(ctx, this.width, this.height / 2, this.width - arrow_height, this.height / 2 - arrow_height / 2);
         this.drawLine(ctx, this.width, this.height / 2, this.width - arrow_height, this.height / 2 + arrow_height / 2);
 
-        this.drawLine(ctx, (0.1 + 0 * 0.8 / 6) * this.width, this.height / 2 - 0.01 * this.height, (0.1 + 0 * 0.8 / 6) * this.width, this.height / 2 + 0.01 * this.height);
-        this.drawLine(ctx, (0.1 + 1 * 0.8 / 6) * this.width, this.height / 2 - 0.01 * this.height, (0.1 + 1 * 0.8 / 6) * this.width, this.height / 2 + 0.01 * this.height);
-        this.drawLine(ctx, (0.1 + 2 * 0.8 / 6) * this.width, this.height / 2 - 0.01 * this.height, (0.1 + 2 * 0.8 / 6) * this.width, this.height / 2 + 0.01 * this.height);
-        this.drawLine(ctx, (0.1 + 4 * 0.8 / 6) * this.width, this.height / 2 - 0.01 * this.height, (0.1 + 4 * 0.8 / 6) * this.width, this.height / 2 + 0.01 * this.height);
-        this.drawLine(ctx, (0.1 + 5 * 0.8 / 6) * this.width, this.height / 2 - 0.01 * this.height, (0.1 + 5 * 0.8 / 6) * this.width, this.height / 2 + 0.01 * this.height);
-        this.drawLine(ctx, (0.1 + 6 * 0.8 / 6) * this.width, this.height / 2 - 0.01 * this.height, (0.1 + 6 * 0.8 / 6) * this.width, this.height / 2 + 0.01 * this.height);
+        // horizontal axis marks
+        for (let i = 0; i <= Canvas.MAX_RADIUS * 2; i ++) {
+            if (i === Canvas.MAX_RADIUS) continue;
+            this.drawLine(ctx, (0.1 + i * 0.8 / (Canvas.MAX_RADIUS * 2)) * this.width, this.height / 2 - 0.01 * this.height,
+                (0.1 + i * 0.8 / (Canvas.MAX_RADIUS * 2)) * this.width, this.height / 2 + 0.01 * this.height);
+        }
 
-        // grid numbers
-        ctx.fillText("3", this.width / 2 + 8, (0.1) * this.height + 6);
-        ctx.fillText("3", this.width - (0.1) * this.width - 6, this.height / 2 - 10);
+        // axis numbers
+        ctx.fillText(Canvas.MAX_RADIUS.toString(), this.width / 2 + 8, (0.1) * this.height + 6);
+        ctx.fillText(Canvas.MAX_RADIUS.toString(), this.width - (0.1) * this.width - 6, this.height / 2 - 10);
     }
 
-    drawLine(ctx, fromx, fromy, tox, toy) {
+    drawLine(ctx, from_x, from_y, tox, toy) {
         ctx.beginPath();
-        ctx.moveTo(fromx, fromy);
+        ctx.moveTo(from_x, from_y);
         ctx.lineTo(tox, toy);
         ctx.stroke();
     }
